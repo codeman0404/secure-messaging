d1ff2488d81aa07c5da6f57096037f0626d438a5
Cody Anderson <cody-anderson@uiowa.edu>
2020-07-31 17:42:32 -0500

    Initial Commit

---
+++ secure-messaging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  secure-messaging
+//
+//  Created by Cody Anderson on 7/31/20.
+//  Copyright © 2020 Cody Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
