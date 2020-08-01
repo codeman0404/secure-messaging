4ad032c0712a9f36afb57a72989767167b2305e3
Lukas Vaughan <lukas.vaughan@gmail.com>
2020-08-01 12:32:20 -0500

    Buttons, Text Fields, Encrypt Function

--- secure-messaging/ViewController.swift
+++ secure-messaging/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pullFromVault(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var messageField: UITextField!
+    
+    
+    @IBAction func pushToVault(_ sender: Any) {
+        
+        if let text = messageField.text {
+            
+            let returnText = encrypt(stringToEncrypt:text)
+            print(returnText)
+            
+        } else {
+            print("please enter a valid message")
+        }
+        
+    }
+    
+    func encrypt (stringToEncrypt:String)->String {
+        
+        return "Hello Bob"
+        
+    }
+    
+    
+    @IBOutlet weak var displayMessages: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
